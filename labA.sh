96b3f8797bbe2aa47485372adf79b2b0e7a852fa
Oladele A. Oluwayiose <ooluwayiose@umass.edu>
2020-04-26 12:05:12 -0400

    Comitting

---
+++ labA.sh
@@ -0,0 +1,52 @@
+Genome_analysis_2020.Rproj      draft.txt.save.1                lab4.html
+LICENSE                         index.Rmd                       lab5.Rmd
+Lab3_HuGen_2020_tutorial.Rmd    index.html                      lab5.html
+MYDRAFTS                        lab1.R                          lab6_rnaseq.Rmd
+README.md                       lab1.Rmd                        lab7_rnaseq.Rmd
+SNP_plot.png                    lab1.html                       paper.Rmd
+airway                          lab2.R                          paper.html
+airway_1.6.0.tar.gz             lab2_report.Rmd                 rnaseqGene.R
+coldata                         lab2_report.html                test.sh
+data                            lab3.Rmd                        tximeta_1.5.27.tar.gz
+draft.txt                       lab3.html
+draft.txt.save                  lab4.Rmd
+1x-nat-vl931-172-30-189-179:Genome_analysis_2020 oladeleoluwayiose$ cp draft.txt draft.txt_new
+1x-nat-vl931-172-30-189-179:Genome_analysis_2020 oladeleoluwayiose$ ls
+Genome_analysis_2020.Rproj      draft.txt.save.1                lab4.Rmd
+LICENSE                         draft.txt_new                   lab4.html
+Lab3_HuGen_2020_tutorial.Rmd    index.Rmd                       lab5.Rmd
+MYDRAFTS                        index.html                      lab5.html
+README.md                       lab1.R                          lab6_rnaseq.Rmd
+SNP_plot.png                    lab1.Rmd                        lab7_rnaseq.Rmd
+airway                          lab1.html                       paper.Rmd
+airway_1.6.0.tar.gz             lab2.R                          paper.html
+coldata                         lab2_report.Rmd                 rnaseqGene.R
+data                            lab2_report.html                test.sh
+draft.txt                       lab3.Rmd                        tximeta_1.5.27.tar.gz
+draft.txt.save                  lab3.html
+1x-nat-vl931-172-30-189-179:Genome_analysis_2020 oladeleoluwayiose$ mv draft.txt_new MYDRAFTS/
+1x-nat-vl931-172-30-189-179:Genome_analysis_2020 oladeleoluwayiose$ cd MYDRAFTS/
+1x-nat-vl931-172-30-189-179:MYDRAFTS oladeleoluwayiose$ ls
+draft.txt.save  draft.txt_new
+1x-nat-vl931-172-30-189-179:MYDRAFTS oladeleoluwayiose$ rm draft.txt.save
+1x-nat-vl931-172-30-189-179:MYDRAFTS oladeleoluwayiose$ ls -a
+.               ..              draft.txt_new
+1x-nat-vl931-172-30-189-179:MYDRAFTS oladeleoluwayiose$ rm draft.txt_new
+1x-nat-vl931-172-30-189-179:MYDRAFTS oladeleoluwayiose$ cd ..
+1x-nat-vl931-172-30-189-179:Genome_analysis_2020 oladeleoluwayiose$ rm -r MYDRAFTS/
+1x-nat-vl931-172-30-189-179:Genome_analysis_2020 oladeleoluwayiose$ ls -a
+.                               airway_1.6.0.tar.gz             lab3.Rmd
+..                              coldata                         lab3.html
+.DS_Store                       data                            lab4.Rmd
+.RData                          draft.txt                       lab4.html
+.Rhistory                       draft.txt.save                  lab5.Rmd
+.Rproj.user                     draft.txt.save.1                lab5.html
+.git                            index.Rmd                       lab6_rnaseq.Rmd
+.gitignore                      index.html                      lab7_rnaseq.Rmd
+Genome_analysis_2020.Rproj      lab1.R                          paper.Rmd
+LICENSE                         lab1.Rmd                        paper.html
+Lab3_HuGen_2020_tutorial.Rmd    lab1.html                       rnaseqGene.R
+README.md                       lab2.R                          test.sh
+SNP_plot.png                    lab2_report.Rmd                 tximeta_1.5.27.tar.gz
+airway                          lab2_report.html
+1x-nat-vl931-172-30-189-179:Genome_analysis_2020 oladeleoluwayiose$
